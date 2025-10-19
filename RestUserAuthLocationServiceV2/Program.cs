2da6d6c1584315e9f74385643ab5324b27382d21
Harrison Ogle <harrisonogle@microsoft.com>
2025-10-18 20:05:13 -0700

    Run Windows service

--- RestUserAuthLocationServiceV2/Program.cs
+++ RestUserAuthLocationServiceV2/Program.cs
@@ -1,4 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddWindowsService(static options => options.ServiceName = "RUALSv2");
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
--- Sandbox111/Create-WindowsService.ps1
+++ Sandbox111/Create-WindowsService.ps1
@@ -6,14 +6,14 @@
   The script creates a new Windows user and configures the user account to be associated
   with an ASP.NET Core application running as a Windows Service.
 
-.PARAMETER ServiceName
+.PARAMETER Name
   The name of the service. This will also be the name of the Windows User.
 
-.PARAMETER ServicePath
+.PARAMETER Path
   The path to the service executable.
 
 .EXAMPLE
-  .\Create-WindowsService.ps1 -ServiceName "RUALSv2"
+  .\Create-WindowsService.ps1 -Name "RUALSv2"
   Creates and configures a Windows user account with the given service name.
 
 .INPUTS
@@ -25,44 +25,44 @@
 
 param(
     [Parameter(Mandatory=$true)]
-    [string]$ServiceName,
+    [string]$Name,
 
     [Parameter(Mandatory=$true)]
-    [string]$ServicePath
+    [string]$Path
 )
 
 $ErrorActionPreference = "Stop"
 
-Write-Host "ServiceName: '$ServiceName'"
-Write-Host "ServicePath: '$ServicePath'"
+Write-Host "Name: '$Name'"
+Write-Host "Path: '$Path'"
 
-if ([string]::IsNullOrEmpty($ServiceName))
+if ([string]::IsNullOrEmpty($Name))
 {
-    Write-Host "Null or empty 'ServiceName' parameter."
+    Write-Host "Null or empty 'Name' parameter."
     exit 1
 }
 
 $serviceNameRegex = [regex]"^[A-Za-z][A-Za-z0-9_]*$"
 
-if ($serviceNameRegex.IsMatch($ServiceName) -eq $false)
+if ($serviceNameRegex.IsMatch($Name) -eq $false)
 {
-    Write-Host "Invalid 'ServiceName' parameter."
+    Write-Host "Invalid 'Name' parameter."
     exit 1
 }
 
-if ([string]::IsNullOrEmpty($ServicePath))
+if ([string]::IsNullOrEmpty($Path))
 {
-    Write-Host "Null or empty 'ServicePath' parameter."
+    Write-Host "Null or empty 'Path' parameter."
     exit 1
 }
 
-if ([System.IO.File]::Exists($ServicePath) -ne $true)
+if ([System.IO.File]::Exists($Path) -ne $true)
 {
-    Write-Host "Service not found at: '$ServicePath'"
+    Write-Host "Service not found at: '$Path'"
     exit 1
 }
 
-$serviceDirectory = [System.IO.Path]::GetDirectoryName($ServicePath)
+$serviceDirectory = [System.IO.Path]::GetDirectoryName($Path)
 
 if ([System.IO.Directory]::Exists($serviceDirectory) -ne $true)
 {
@@ -71,69 +71,42 @@ if ([System.IO.Directory]::Exists($serviceDirectory) -ne $true)
 }
 
 # Create the Windows user.
-Write-Host "Creating Windows user with name: '$ServiceName'"
+Write-Host "Creating Windows user with name: '$Name'"
 $password = ConvertTo-SecureString -String (-join ((33..126) | Get-Random -Count 32 | % {[char]$_})) -AsPlainText -Force
-$user = New-LocalUser -Name "$ServiceName" -Password $password -AccountNeverExpires -FullName "$ServiceName" -Description "$ServiceName"
+$user = New-LocalUser -Name "$Name" -Password $password -AccountNeverExpires -FullName "$Name" -Description "$Name"
 Write-Host "Created new user: $user"
 
-Write-Host "Searching for user '$ServiceName'."
-$found = net user "$ServiceName"
+Write-Host "Searching for user '$Name'."
+$found = net user "$Name"
 
 if ($LASTEXITCODE -ne 0)
 {
     $exitCode = $LASTEXITCODE # formatting purposes
-    Write-Host "Error finding user with name '$ServiceName'. (exit code: $exitCode)"
+    Write-Host "Error finding user with name '$Name'. (exit code: $exitCode)"
     exit 1
 }
 else
 {
-    Write-Host "Found user with name '$ServiceName'."
+    Write-Host "Found user with name '$Name'."
     [System.String]::Join("`n", $found) | Write-Host
 }
 
 # Give the user "logon as a service" permissions so it can do network stuff.
-Write-Host "Adding `"logon as a service`" permission to user '$ServiceName'."
-& ".\Add-ServiceLogonRight.ps1" "$ServiceName"
-Write-Host "Added `"logon as a service`" permission to user '$ServiceName'."
+Write-Host "Adding `"logon as a service`" permission to user '$Name'."
+& ".\Add-ServiceLogonRight.ps1" "$Name"
+Write-Host "Added `"logon as a service`" permission to user '$Name'."
 
 # ACL the service executable to the file system.
 Write-Host "ACL'ing the service executable to the file system."
-$acl = Get-Acl "$ServicePath"
-# $aclRuleArgs = "$ServiceName", "Read,Write,ReadAndExecute", "ContainerInherit,ObjectInherit", "None", "Allow"
-$aclRuleArgs = "$ServiceName", "Read,Write,ReadAndExecute", "None", "None", "Allow"
+$acl = Get-Acl "$Path"
+$aclRuleArgs = "$Name", "Read,Write,ReadAndExecute", "None", "None", "Allow"
 $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($aclRuleArgs)
-do
-{
-    $attempt = 0
-    $check = 'ok'
-    try
-    {
-        $acl.SetAccessRule($accessRule)
-    }
-    catch [System.Management.Automation.RuntimeException]
-    {
-        $_.Exception.Message
-        $check = 'error'
-        Start-Sleep -Seconds 2
-        $attempt = $attempt + 1
-
-        if ($attempt -ge 5)
-        {
-            Write-Host "Error setting filesystem ACL."
-            Write-Host $_
-            exit 1
-        }
-    }
-} until (
-    $check -eq 'ok'
-)
-# $acl.SetAccessRule($accessRule)
-$acl | Set-Acl "$ServicePath"
+$acl.SetAccessRule($accessRule)
+$acl | Set-Acl "$Path"
 Write-Host "ACL'ed the service executable to the file system."
 
 # Create the Windows service.
 Write-Host "Creating Windows service."
-$serviceCredential = [System.Management.Automation.PSCredential]::new(".\$ServiceName", $password)
-New-Service -Name "$ServiceName" -BinaryPathName "$ServicePath --contentRoot $serviceDirectory" -Credential $serviceCredential -Description "$ServiceName" -DisplayName "$ServiceName" -StartupType Manual
-# New-Service -Name "$ServiceName" -BinaryPathName "$ServicePath --contentRoot $serviceDirectory" -Credential "$ServiceName" -Description "$ServiceName" -DisplayName "$ServiceName" -StartupType Automatic
+$serviceCredential = [System.Management.Automation.PSCredential]::new(".\$Name", $password)
+New-Service -Name "$Name" -BinaryPathName "$Path --contentRoot $serviceDirectory" -Credential $serviceCredential -Description "$Name" -DisplayName "$Name" -StartupType Manual
 Write-Host "Created Windows service."
